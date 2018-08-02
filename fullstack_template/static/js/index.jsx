363b5dd64d09695aef12040dc83c57424b23fafe
Karn Rahal <karn.rahal@alumni.ubc.ca>
2018-08-01 19:14:29 -0700

    Removed React and fullstack structure to start from scratch

--- fullstack_template/static/js/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-// Load a React class we have created in a separate App.jsx file
-ReactDOM.render(<App />, document.getElementById("content"));
